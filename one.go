3e4652005173cb115c80d739433f2805fbb99283
mrfarhankhan007 <fk95034@gmail.com>
2024-08-05 20:12:47 +0530

    first commit

---
+++ one.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// import "rsc.io/quote"
+
+func main() {
+	// // fmt.Println("Hello World");
+	// fmt.Println(quote.Go())
+	// fmt.Println(quote.Hello())
+
+	//strings
+	a := "Farhan"
+	fmt.Print(a)
+	fmt.Print(reflect.TypeOf(a))
+
+}
